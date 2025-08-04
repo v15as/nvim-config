5cbd38454381f1aee7fa84a67876427918f8d2e2
João Vivas <filipe.vivas.fv@gmail.com>
2025-08-04 01:39:11 +0100

    Added todo comment highlighting plugin in my config

--- lua/settings/keybinds.lua
+++ lua/settings/keybinds.lua
@@ -68,3 +68,11 @@ map('n','<C-n>','<CMD>NvimTreeToggle<Cr>')
 map('n','<A-q>','<CMD>tabNext<Cr>')
 map('t','<A-q>','<CMD>tabNext<Cr>')
 
+
+vim.keymap.set("n", "]t", function()
+  require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+  require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
